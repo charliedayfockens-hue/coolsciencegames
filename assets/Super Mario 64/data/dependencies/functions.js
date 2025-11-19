a0932d083a50acd90e0712870e92dfa554088485
charliedayfockens-hue <charliedayfockens@gmail.com>
2025-11-18 21:00:34 -0600

    Add files via upload

---
+++ assets/Super Mario 64/data/dependencies/functions.js
@@ -0,0 +1,33 @@
+function GEO_SHADOW(t, c, o) {
+    return t, c, o;
+}
+
+function GEO_OPEN_NODE() {
+    return false;
+}
+
+function GEO_SCALE(u, s) {
+    return u, s;
+}
+
+function GEO_ANIMATED_PART(l, x, y, z, dl) {
+    return l, x, y, z, dl;
+}
+
+function GEO_SWITCH_CASE(c, n) {
+    return c, n;
+}
+
+function GEO_CLOSE_NODE() {
+    return false;
+}
+
+function GEO_BILLBOARD() {
+    return false;
+}
+
+function GEO_END() {
+    return false;
+}
+
+export { functions }
